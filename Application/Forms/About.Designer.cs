cb64b6f37b0608a9ca26f1a3f8eb5559f440c5f5
shellscape <andrew@shellscape.org>
2012-09-23 12:06:50 -0400

    adding about window

---
+++ Application/Forms/About.Designer.cs
@@ -0,0 +1,100 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+using Microsoft.WindowsAPI.Dialogs;
+using Microsoft.WindowsAPI.Shell;
+
+namespace SimplePowerPlus.Forms {
+	partial class About {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this._Flow = new System.Windows.Forms.FlowLayoutPanel();
+			this._LabelCopyright = new Shellscape.UI.Controls.ResponsiveLinkLabel();
+			this._Flow.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// _Button
+			// 
+			this._Button.Location = new System.Drawing.Point(435, 206);
+			// 
+			// _ButtonDonate
+			// 
+			this._ButtonDonate.Location = new System.Drawing.Point(3, 206);
+			// 
+			// _Flow
+			// 
+			this._Flow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._Flow.BackColor = System.Drawing.Color.Transparent;
+			this._Flow.Controls.Add(this._LabelCopyright);
+			this._Flow.Location = new System.Drawing.Point(3, 152);
+			this._Flow.Name = "_Flow";
+			this._Flow.Size = new System.Drawing.Size(519, 43);
+			this._Flow.TabIndex = 2;
+			// 
+			// _LabelCopyright
+			// 
+			this._LabelCopyright.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(74)))), ((int)(((byte)(229)))));
+			this._LabelCopyright.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._LabelCopyright.AutoSize = true;
+			this._LabelCopyright.DisabledLinkColor = System.Drawing.Color.Gray;
+			this._LabelCopyright.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(74)))), ((int)(((byte)(229)))));
+			this._LabelCopyright.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+			this._LabelCopyright.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(28)))), ((int)(((byte)(85)))));
+			this._LabelCopyright.Location = new System.Drawing.Point(3, 0);
+			this._LabelCopyright.Name = "_LabelCopyright";
+			this._LabelCopyright.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(28)))), ((int)(((byte)(85)))));
+			this._LabelCopyright.Size = new System.Drawing.Size(60, 15);
+			this._LabelCopyright.TabIndex = 0;
+			this._LabelCopyright.TabStop = true;
+			this._LabelCopyright.Text = "Copyright";
+			this._LabelCopyright.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(28)))), ((int)(((byte)(85)))));
+			// 
+			// About
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(526, 251);
+			this.Controls.Add(this._Flow);
+			this.Name = "About";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.TopMost = false;
+			this.Controls.SetChildIndex(this._ButtonDonate, 0);
+			this.Controls.SetChildIndex(this._Button, 0);
+			this.Controls.SetChildIndex(this._Flow, 0);
+			this._Flow.ResumeLayout(false);
+			this._Flow.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private FlowLayoutPanel _Flow;
+		private Shellscape.UI.Controls.ResponsiveLinkLabel _LabelCopyright;
+	}
+}
---
+++ Application/Forms/About.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+using Microsoft.WindowsAPI.Dialogs;
+using Microsoft.WindowsAPI.Shell;
+
+
+using Shellscape;
+
+namespace SimplePowerPlus.Forms {
+	public partial class About : Shellscape.UI.About {
+
+		private Bitmap _logo;
+
+		public About() : base() {
+
+			this.Icon = Resources.Icons.Window;
+			this.Text = "About - Simple Power Plus";
+			this._logo = Resources.Images.IconLarge;
+
+			InitializeComponent();
+
+			this._Flow.SuspendLayout();
+			this.SuspendLayout();
+
+			String toYear = DateTime.Now.Year.ToString();
+			String linkTarget = "Shellscape Software";
+
+			toYear = toYear == "2012" ? String.Empty : String.Concat("-", toYear);
+
+			this._Button.Font = this._ButtonDonate.Font = SystemFonts.MessageBoxFont;
+			this._Button.Text = "Sweet, Thanks!";
+			this._ButtonDonate.Text = "Please consider donating";
+			this._LabelCopyright.Text = String.Join("\n",
+				String.Concat("Copyright © 2012", toYear, " Andrew Powell, ", linkTarget, ". All rights reserved."));
+			this._LabelCopyright.Links.Add(this._LabelCopyright.Text.IndexOf(linkTarget), linkTarget.Length);
+			this._LabelCopyright.Font = SystemFonts.MessageBoxFont;
+			this._LabelCopyright.LinkColor = this._LabelCopyright.NormalColor = this._LabelCopyright.HoverColor;
+			this._LabelCopyright.LinkBehavior = LinkBehavior.AlwaysUnderline;
+
+			this._Flow.ResumeLayout(true);
+			this.ResumeLayout(true);
+
+			this._Button.Click += delegate(object sender, EventArgs e) {
+				this.Close();
+			};
+
+			this._LabelCopyright.LinkClicked += delegate(object sender, LinkLabelLinkClickedEventArgs e) {
+				Help.ShowHelp(this, "http://shellscape.org");
+			};
+		}
+
+		protected override string DonationDescription {
+			get { return "Simple%20Power%20Plus%20Donation"; }
+		}
+
+		protected override void OnPaintIcon(Graphics g) {
+			float percent = (float)256 / (float)this._logo.Width;
+			g.DrawImage(this._logo, 326, 7, this._logo.Width * percent, this._logo.Height * percent);
+		}
+
+	}
+}
