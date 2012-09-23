1344327626393a6b4068aea1a8dc62d47cde6eb1
shellscape <andrew@shellscape.org>
2012-09-23 12:06:09 -0400

    updating form reference

--- Application/Program.cs
+++ Application/Program.cs
@@ -13,7 +13,7 @@ namespace SimplePowerPlus {
 	static void Main() {
 	  Application.EnableVisualStyles();
 	  Application.SetCompatibleTextRenderingDefault(false);
-	  Application.Run(new Main());
+	  Application.Run(new Forms.Main());
 	}
   }
 }
